e425bfdc8d5e24376895f50ae286924da12e6759
Emmanouil Kiagias <ekiagias@pivotal.io>
2017-07-18 18:13:06 +0100

    Initial incomplete list-namespaces implementation
    Signed-off-by: Will Martin <wmartin@pivotal.io>

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	ps "github.com/mitchellh/go-ps"
 	"github.com/urfave/cli"
 )
 
@@ -12,6 +13,7 @@ func main() {
 
 	nsjam.Commands = []cli.Command{
 		Version,
+		ListNamespaces,
 	}
 
 	_ = nsjam.Run(os.Args)
@@ -22,6 +24,23 @@ var Version = cli.Command{
 	Action: func(ctx *cli.Context) error {
 		fmt.Println("0.0.1")
 
+		return nil
+	},
+}
+
+var ListNamespaces = cli.Command{
+	Name: "list-namespaces",
+	Flags: []cli.Flag{
+		cli.BoolFlag{
+			Name: "pid",
+		},
+		cli.IntFlag{
+			Name: "target",
+		},
+	},
+	Action: func(ctx *cli.Context) error {
+		process, _ := ps.FindProcess(ctx.Int("target"))
+		fmt.Println(process.Executable())
 		return nil
 	},
 }
---
+++ marley/list_namespaces_test.go
@@ -0,0 +1,55 @@
+package cqt_test
+
+import (
+	"os/exec"
+	"strconv"
+	"syscall"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("ListNamespaces", func() {
+
+	var (
+		session *gexec.Session
+		cmd     *exec.Cmd
+
+		targetNS string
+	)
+
+	BeforeEach(func() {
+		targetNS = ""
+	})
+
+	JustBeforeEach(func() {
+		cmd = exec.Command("sleep", "60")
+		Expect(cmd.Start()).To(Succeed())
+
+		session = execNSJam("list-namespaces", targetNS, "--target", strconv.Itoa(cmd.Process.Pid))
+	})
+
+	AfterEach(func() {
+		Expect(cmd.Process.Signal(syscall.SIGKILL)).To(Succeed())
+	})
+
+	Context("when listing the pid namespace", func() {
+		BeforeEach(func() {
+			targetNS = "--pid"
+		})
+
+		It("exits with a zero exit code", func() {
+			Eventually(session).Should(gexec.Exit(0))
+		})
+
+		It("prints out the name of the target", func() {
+			Eventually(session).Should(gbytes.Say("sleep"))
+		})
+
+		// It("prints out the pid namespace of the target", func() {
+		// 	Expect(string(session.Wait().Out.Contents())).To(MatchRegexp(`mnt:\d+`))
+		// })
+	})
+})
