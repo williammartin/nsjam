2bd4ba1612b927f42153f76c9d50e17a01b77a37
Emmanouil Kiagias <ekiagias@pivotal.io>
2017-07-18 17:31:30 +0100

    Implement version command
    Signed-off-by: Will Martin <wmartin@pivotal.io>

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
-type command struct {
-	name        string
-	description string
-	command     interface{}
-}
+import (
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli"
+)
 
 func main() {
+	nsjam := cli.NewApp()
+
+	nsjam.Commands = []cli.Command{
+		Version,
+	}
+
+	_ = nsjam.Run(os.Args)
+}
+
+var Version = cli.Command{
+	Name: "version",
+	Action: func(ctx *cli.Context) error {
+		fmt.Println("0.0.1")
+
+		return nil
+	},
 }
---
+++ marley/version_test.go
@@ -0,0 +1,27 @@
+package cqt_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("Version", func() {
+
+	var (
+		session *gexec.Session
+	)
+
+	JustBeforeEach(func() {
+		session = execNSJam("version")
+	})
+
+	It("exits with a zero exit code", func() {
+		Eventually(session).Should(gexec.Exit(0))
+	})
+
+	It("reports the semver", func() {
+		Eventually(session.Out).Should(gbytes.Say("0.0.1"))
+	})
+})
